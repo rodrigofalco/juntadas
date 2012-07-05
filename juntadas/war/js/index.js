7dc1ba43d1ea71b61083d4df545623db2cf2cb33
David Vydra <david@vydra.net>
2012-07-04 21:00:09 -0300

    Agregado datatable.

---
+++ juntadas/war/js/index.js
@@ -0,0 +1,11 @@
+
+/* Table initialisation */
+$(document).ready(function() {
+	$('#juntadasTable').dataTable( {
+		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType": "bootstrap",
+		"oLanguage": {
+			"sLengthMenu": "_MENU_ records per page"
+		}
+	} );
+} );
